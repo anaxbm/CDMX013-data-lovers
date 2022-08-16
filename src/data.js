e92b4f364bfeaf76749dc0800574d4862c7cd24d
anankebj <anankebj@gmail.com>
2022-08-16 12:08:13 -0500

    css styles changed

--- src/data.js
+++ src/data.js
@@ -1,11 +1,12 @@
 // estas funciones son de ejemplo
 import data from "./data/ghibli/ghibli.js";
 const { films } = data;
+/*deconstruccion, extrayendo films del objeto data del archivo ghibli.js */
 export const posters = films.map(function (x) {
   /*                             object    */
   return `
   <div class="poster">
-  <h2> ${x.title}  </h2>
+  <h2> ${x.title} </h2>
   <h3> ${x.release_date} </h3>
     <img class= "movie-image" src="${x.poster}" alt="poster" srcset="">
   </div>`;
